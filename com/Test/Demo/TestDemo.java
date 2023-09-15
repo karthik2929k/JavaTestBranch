f1418fe3fbd10c7434b2384c24b52f346a651322
karthik2929k <penchalakarthik9@gmail.com>
2023-09-15 11:56:58 +0530

    second commit

--- com/Test/Demo/TestDemo.java
+++ com/Test/Demo/TestDemo.java
@@ -1,9 +1,15 @@
 package com.Test.Demo;
 
+import java.util.Date;
+import java.util.function.Supplier;
+
 public class TestDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello karthik !");
+		Supplier<Date> s= ()->new Date();
+		Date d2=new Date();
+		System.out.println(s.get());
 	}
 }
