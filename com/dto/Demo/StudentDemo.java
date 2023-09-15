e633e44e41c1aa348bb34b6450c172efab4386d7
karthik2929k <penchalakarthik9@gmail.com>
2023-09-15 12:26:44 +0530

    Third commit

---
+++ com/dto/Demo/StudentDemo.java
@@ -0,0 +1,20 @@
+package com.dto.Demo;
+
+public class StudentDemo {
+	
+	String name;
+	int Rollno;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRollno() {
+		return Rollno;
+	}
+	public void setRollno(int rollno) {
+		Rollno = rollno;
+	}
+	
+}
