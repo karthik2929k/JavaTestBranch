7ea15b1c246e27463166e92197338024ebf11a00
karthik2929k <penchalakarthik9@gmail.com>
2023-09-15 12:29:27 +0530

    forth commit

--- com/dto/Demo/StudentDemo.java
+++ com/dto/Demo/StudentDemo.java
@@ -2,13 +2,15 @@ package com.dto.Demo;
 
 public class StudentDemo {
 	
-	String name;
+	String name2;
 	int Rollno;
-	public String getName() {
-		return name;
+	
+	
+	public String getName2() {
+		return name2;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setName2(String name2) {
+		this.name2 = name2;
 	}
 	public int getRollno() {
 		return Rollno;
